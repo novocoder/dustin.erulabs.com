4543b83e7a437a6b32a8125d9c586900f3f1fe34
Dustin <lloyd.dustin@gmail.com>
2014-11-20 07:06:39 +0000

    fix merge

--- index.php
+++ index.php
@@ -37,40 +37,13 @@
 		
 	</form>
 
-
 	</section>
 
-<<<<<<< HEAD
-
-
-
-
-
-=======
->>>>>>> 9428d697c41660560d357a6f6c5c8aeb8c67ac84
 <pre>
 
 <?php
-
-
-<<<<<<< HEAD
-// Include the PHP RIOT API library
-include('php-riot-api.php');
-
-// Start the library with the 'na' region code
-$lolapi = new riotapi('na');
-
-// IF they DIDNT provided a name in the URL, like "/?name=bob", then use dustins name
-if ($_POST['name'] == '') {
-	$name = 'Reptuar';
-// Otherwise, use what they gave us
-} else {
-	$name = $_POST['name'];
-}
-=======
 require 'vendor/autoload.php';
 use LeagueWrap\Api;
->>>>>>> 9428d697c41660560d357a6f6c5c8aeb8c67ac84
 
 $api = new Api('9249495a-ec4c-4026-bb9c-a7648103bd41');
 
