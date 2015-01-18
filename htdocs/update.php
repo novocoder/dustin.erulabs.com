7297eb147399223c21332f4ac77e91fe6a10eaac
root <root@dustin.erulabs.com>
2015-01-17 23:46:16 -0600

    more automations

--- htdocs/update.php
+++ htdocs/update.php
@@ -1,25 +1,24 @@
 <?PHP
 
-error_reporting(0);
+#error_reporting(0);
 
 
-require 'secretFile.php';
+require '../secretFile.php';
 
-list($algo, $hash) = explode('=', $_SERVER['X-Hub-Signature']);
+list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE']);
 $payload = file_get_contents('php://input');
-$data - json_decode($payload);
+$data = json_decode($payload);
 
 $payloadHash = hash_hmac($algo, $payload, $secret);
 
 
-if ($hash[ !== $payloadHash) {
+if ($hash !== $payloadHash) {
   header("HTTP/1.0 404 Not Found");
   exit;
 }
 
-echo '<pre>';
-#echo exec("whoami");
-echo exec("sudo salt-call --local state.sls base");
-echo '</pre>';
+exec("sudo salt-call --local state.sls base 2>&1", $output);
+
+print_r($output);
 
 ?>
